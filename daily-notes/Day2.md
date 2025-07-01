8a5aee0d8675d22c5ed113d172c27662cdaed6b6
Tanishq Yadav <tannu.yadav2310@gmail.com>
2025-07-01 11:47:05 +0530

    Created daily-notes of Day2 and created js folder and Js basics

---
+++ daily-notes/Day2.md
@@ -0,0 +1,5 @@
+# Day2:
+
+- Brushed up the basics of js 
+- Leaned how to make functions and how to use them or call them.
+
---
+++ mern-cohort/js/js-basics/index.js
@@ -0,0 +1,10 @@
+let a = 23; // we change the value of this afterwards when we want
+const b = 12; // we cannot change its value after it is declared and assigned the value
+var c = 39; // older way of ddeclaring variables
+
+//Functions in js
+function greet(name){
+    return "Hello " + name;
+}
+
+let messagge = greet("Tanishq");
