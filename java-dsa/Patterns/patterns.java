6325b9c9528cbf74df495f3bc82c7b327b214a49
Tanishq Yadav <tannu.yadav2310@gmail.com>
2025-07-03 21:33:32 +0530

    Paterns in java

--- java-dsa/Patterns/patterns.java
+++ java-dsa/Patterns/patterns.java
@@ -1,7 +1,8 @@
 public class patterns {
     public static void main(String[] args){
     //pattern1(5);
-    pattern2(5);
+    //pattern2(5);
+    pattern3(5);
     }
     // pattern1
     /*
@@ -27,7 +28,23 @@ public class patterns {
      */
     static void pattern2(int n){
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
+            for (int j = n-1; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    /* pattern3
+     * * * * *
+     * * * *
+     * * *
+     * *
+     *
+     */
+    static void pattern3(int n){
+        for (int i = n; i >= n; i--) {
+            for (int j = 0; j < n-1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
