5c566e7415153baf1736b7c84f4b8ca72748f31b
Tanishq Yadav <tannu.yadav2310@gmail.com>
2025-07-05 09:48:46 +0530

    Updated previous day work and created daily-notes

---
+++ daily-notes/Day6.md
@@ -0,0 +1,18 @@
+# Day6 05/07/2025
+
+## What I did today:
+
+# MERN Cohort:-
+- 
+- 
+- 
+-
+-
+
+# Java:-
+-
+-
+-
+-
+
+
--- mern-cohort/js/dom/index.js
+++ mern-cohort/js/dom/index.js
@@ -2,5 +2,12 @@ function addTodo(){
     const inputElement = document.querySelector("input");
     const value = inputElement.value;
     console.log(value);
-    // in same way we can use getEkementById and getElementByClass if we wnt to get any tag or element by its class or id.
+    // in same way we can use getEkementById and getElementByClassName  if we wnt to get any tag or element by its class or id.
 }
+let cntr = 0;
+function callback(){
+    const tag = document.querySelector(".counter");
+    tag.innerHTML = cntr;
+    //cntr++; uncomment it when to use either it will run forever
+}
+setInterval(callback, 1000);
