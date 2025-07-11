0c40418b11cf59bfa23ed9afb4ec425a6b3ced32
Tanishq Yadav <tannu.yadav2310@gmail.com>
2025-07-11 19:22:06 +0530

    learning nodejs

---
+++ mern-cohort/node-js/node-practice/index.mjs
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+function addTwoNum(a, b){
+    return(a+b);
+}
+console.log(addTwoNum(3,4));
+console.log(chalk.blue.bold("This is chalk "));
+console.log(chalk.red("hello world"));
