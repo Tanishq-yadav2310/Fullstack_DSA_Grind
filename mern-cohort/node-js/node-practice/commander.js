fd919f76438f6f2b710417e1e92032072c57eb08
Tanishq Yadav <tannu.yadav2310@gmail.com>
2025-07-12 22:11:12 +0530

    learned commander package of nodejs

--- mern-cohort/node-js/node-practice/commander.js
+++ mern-cohort/node-js/node-practice/commander.js
@@ -1,2 +1,25 @@
 // using cli package commander to create cli 
-const { command } = require('commander');
+const { Command } = require('commander');
+const program = new command;
+const fs = require('fs');
+
+program
+    .name('Counter app')
+    .description('CLI to do file based tasks')
+    .version('0.8.0');
+
+program.command('count')
+    .description('Count the number of words in a file')
+    .argument('<file>', 'input the file path')
+    .action((file) => {
+        fs.readFile(file, 'utf-8', (err, data) =>{
+            if(err){
+                console.log(err);
+            }else{
+                const words = data.split(" ").length
+                console.log(`there are ${ words + 1} words in ${file}`)
+            }
+        });
+    });
+
+program.parse();
--- mern-cohort/node-js/node-practice/index.mjs
+++ mern-cohort/node-js/node-practice/index.mjs
@@ -7,3 +7,4 @@ console.log(addTwoNum(3,4));
 console.log(chalk.blue.bold("This is chalk "));
 console. log(chalk.red("hello world"));
 console.log
+
