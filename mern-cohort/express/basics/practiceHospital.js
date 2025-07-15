bb48dbe5ea56f0f70ca65771070847eaf8083a58
Tanishq Yadav <tannu.yadav2310@gmail.com>
2025-07-15 20:21:28 +0530

    updated practice hospital backend code

--- mern-cohort/express/basics/practiceHospital.js
+++ mern-cohort/express/basics/practiceHospital.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const app = express();
 
-
+const users = [{
+    name : "john",
+    kidneys : [{
+        healthy: false
+    }]
+}]
 app.get('/', function(req,res){
-
+    const johnKidneys = users[0].kidneys;// gives an array of number of kidneys
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for(let i = 0; i<numberOfKidneys; i++){
+        if(numberOfKidneys){
+            numberOfHealthyKidneys++;
+        }
+    }
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
 })
 
 app.listen(3000);
